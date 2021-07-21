bb785c2b2758987115f42e49a31b493e0fc45e16
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-21 18:22:54 +0800

    Brew formula update for httpcase version v0.0.6

--- Formula/httpcase.rb
+++ Formula/httpcase.rb
@@ -5,29 +5,29 @@
 class Httpcase < Formula
   desc "接口自动化测试工具"
   homepage "https://github.com/roseboy/httpcase"
-  version "0.0.5"
+  version "0.0.6"
   license "MulanPSL2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/roseboy/httpcase/releases/download/v0.0.5/httpcase_0.0.5_darwin_x86_64.tar.gz"
-      sha256 "7253f30e527989e6e369a42ef9f14d134230b15fd6833e0e06d46668f1e29059"
+      url "https://github.com/roseboy/httpcase/releases/download/v0.0.6/httpcase_0.0.6_darwin_x86_64.tar.gz"
+      sha256 "0c72b3c7e1a07c6dacf4ba483811868df85542d1b34f194aa29367758ab38625"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/roseboy/httpcase/releases/download/v0.0.5/httpcase_0.0.5_linux_x86_64.tar.gz"
-      sha256 "68f1df8cacb045d219244394a42917eec633f6d35020dffcceace198259b7ba3"
+      url "https://github.com/roseboy/httpcase/releases/download/v0.0.6/httpcase_0.0.6_linux_x86_64.tar.gz"
+      sha256 "1fa58e84aaabac5bb78c803980fa6d7f5a340f5b6e236dce99e560d35fe4f90d"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/roseboy/httpcase/releases/download/v0.0.5/httpcase_0.0.5_linux_armv6.tar.gz"
-      sha256 "3043beab0bc7924d20c6a0e00dc598f75bfb636ea471ab88d9ec26c48b0b7859"
+      url "https://github.com/roseboy/httpcase/releases/download/v0.0.6/httpcase_0.0.6_linux_armv6.tar.gz"
+      sha256 "54a2696ec230de09600c684ee9bb6477fef1176277ecee5a47963e3112e0bc7a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/roseboy/httpcase/releases/download/v0.0.5/httpcase_0.0.5_linux_arm64.tar.gz"
-      sha256 "73c1671a76eb2685381810ce9891a269177ac104b58214a06384ef945b1ccccf"
+      url "https://github.com/roseboy/httpcase/releases/download/v0.0.6/httpcase_0.0.6_linux_arm64.tar.gz"
+      sha256 "a087380686a159dacecad88ebd7896d59fe8e44920d7eeab43c3990fe2f27dfe"
     end
   end
 
